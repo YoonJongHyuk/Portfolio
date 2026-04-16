87d79dea438cb299eb3b8de45e4a2a7a134c9194
YoonJongHyuk <mana951@naver.com>
2026-04-16 17:59:44 +0900

    Add files via upload

---
+++ CleanOceanCompany/Mediapipe/MediapipeThirdEventController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MediapipeThirdEventController : MonoBehaviour
+{
+
+    public MediapipeThirdManager mediapipeThirdManager;
+
+    public MediapipeThirdTimeManager mediapipeThirdTimeManager;
+
+    public Animator animator;
+
+    public Interaction_UICamera_Controller interaction_UICamera;
+    public Camera mediapipeCamera;
+
+    public GameObject mediapipeCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
